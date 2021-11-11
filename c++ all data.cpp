1ea0854524411ca006bfcc68632cb71ed6581bed
ranaxherry <ranaherry3@gmail.com>
2021-11-11 01:33:48 -0800

    add files

---
+++ c++ all data.cpp
@@ -0,0 +1,1443 @@
+
+//{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{[basics}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}]
+//basic
+//cout<<"ahmad";
+//int a=10;
+//cout<<a;
+//int a,b,c;
+
+//cout<<"Enter First Number=";
+//cin>>a;
+
+/* cout<<"First Number is="<<a<<endl;
+cout<<"Enter Second Number=";
+cin>>b;
+cout<<"Second number is="<<b<<endl;
+c=a+b;
+cout<<"Sum is="<<c;
+**/
+
+/*int a;
+a=10;
+a=20;
+cout<<a<<endl;
+cout<<"Enter number="<<endl;
+cin>>a;
+cout<<a<<endl;
+
+**/
+
+/*int a;
+cout<<"enter number="<<endl;
+cin>>a;
+cout<<"value of a="<<a<<endl;
+**/
+
+
+
+
+}
+
+
+	/*{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{strings + switch}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+	
+	
+	
+	string a;
+	a="ahmad";
+	cout<<"enter your name=";
+	getline (cin, a);
+	cout<<"value in a="<<a<<endl;
+	*/
+/*int a=10;
+int b=10;
+if(a==b)
+{
+	cout<<"both are equal";
+}
+*/
+/*string a="Pakistan";
+string b="Capital";
+string c=a+ " " +b;
+cout<<c;
+*/
+
+/*string a="10";
+string b="20";
+string c=a+b;
+cout<<c;
+*/
+
+/*string a="10";
+string b="pakistan";
+string c=b+a;
+cout<<c;
+*/
+
+/*bool electricity=false;
+cout<<electricity;
+	*/
+	
+/*	int a=10;
+	int b=20;
+	
+	if (a!=b)
+	{
+	cout<<"a is less than b";
+	}
+	*/
+	
+/*	int a=10;
+	int b=-1;
+	if(!(a>0 || b>0))
+	{
+		cout<<"a or b is postive";
+	}
+	else
+	{
+		cout<<"this part";
+	}
+	*/
+	
+/*	int a=15;
+	int b=15;
+	
+	if(a>b)
+	{
+	cout<<"a is less than b";	
+	}
+	else if(a==b)
+	{
+		cout<<"a is equal to b";
+	}
+	else
+	{
+		cout<<"b is greater than a";
+	}*/
+	
+/*	int marks;
+	cout<<"Enter marks=";
+	cin>>marks;
+	if (marks>80)
+	{
+		cout<<"A grade";
+		
+	}
+	
+	else if(marks>70)
+	{
+		cout<<"B grade";
+	}
+	else
+	{
+		cout<<"fail";
+	}*/
+	
+/*	char a;
+	cout<<"Enter first character of color=";
+	cin>>a;
+	
+	switch(a)
+	{
+		case 'y':
+			cout<<"Color is yellow";
+			break;
+		case 'w':
+			cout<<"color is white";
+			break;
+		case 'b':
+			cout<<"color is black";
+			break;
+	*/
+
+
+
+/*{{{{{{{{{{{{{{{{{{{{{{{{{{{{loooopss}}}}}}}}}}}}}}]
+
+
+	cout<<"shehraiz";
+	cout<<"shehraiz";
+	cout<<"shehraiz";
+	cout<<"shehraiz";
+	cout<<"shehraiz";
+	cout<<"shehraiz";
+	cout<<"shehraiz";
+	cout<<"shehraiz";
+	cout<<"shehraiz";
+	cout<<"shehraiz";
+	*/
+/*	for (int i=10; i<20; i++)
+	{
+		cout<<"Shehraiz"<<endl;
+		
+	}
+	*/
+	
+	
+/*	int a;
+	cout<<"Enter the last number you want to print=";
+	cin>>a;
+	
+	for(int i=1;i<=a;i++)
+	{
+		cout<<i<<endl;
+	}
+	*/
+/*	int a;
+cout<<"Enter number o which you want table=";
+cin>>a;
+for(int i=1; i<=10; i++)
+{
+	int b;
+	b=a*i;
+	cout<<a<<"*"<<i<<"="<<b<<endl;
+}
+*/
+
+/*int i=0;
+while(i<5)
+{
+	cout<<i;
+	i++;
+}
+*/
+
+
+/*int sum=0;
+for (int i=1;i<=10;i++)
+{	
+	sum =sum+i;
+	
+	
+}
+cout<<"sum is="<<sum<<endl;
+
+}
+*/
+
+/*int sum=0;
+int i=1;
+while(i<=10)
+{
+	sum = sum+i;
+	i++;
+}
+
+cout<<"sum is"<<sum<<endl;
+*/
+
+
+}
+
+
+/*{{{{{{{{{{{{{{{{{{for loop{{{{{{{{{{{{{{{{{{{{{{[}}}}}}}}}]
+	int a,b,c;
+	cout<<"Enter First Value=";
+	cin>>a;
+	cout<<"Enter Second value=";
+	cin>>b;
+	cout<<"Enter third value=";
+	cin>>c;
+	if(a==b)
+	{
+		if(a==c)
+		{
+			cout<<"Numbers are equal";
+		}
+		else
+		{
+			cout<<"Numbers are not equal";
+		}
+	}
+	else
+	{
+		cout<<"All numbers are not equal";
+	}*/
+	
+	
+/*	for(int i=1;i<5;i++)
+	{
+		for(int j=1;j<5;j++)
+		{
+		cout<<i<<","<<j<<endl;
+		}
+
+	}*/
+	
+	
+/*	for(int i=1;i<=3;i++)
+	{
+		for(int j=1;j<=4;j++)
+		{
+			cout<<"*";
+		}
+	}*/
+	
+	
+/*	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			cout<<j;
+		}
+		cout<<endl;
+	}*/
+	
+	
+	
+}
+
+
+
+/*{{{{{{{{{{{{{{{{{{{{{{{{{{do while loop}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+
+	for(int i=1;i<1;i++)
+	{
+		cout<<i;
+	}
+	cout<<"Loop terminate"<<endl;*/
+	
+	
+	
+/*	int i=1;
+	do
+	{
+	cout<<i<<endl;
+	i++;
+	}
+	while(i<1);*/
+	
+	
+	
+/*	int a;
+	cout<<"Enter Value of which you want to table";
+	cin>>a;
+	int i=1;
+	do
+	{
+		int c;
+		c=a*i;
+		cout<<a<<"*"<<i<<"="<<c<<endl;
+		i++;
+	}
+	
+	while(i<=10);
+	*/
+	
+	
+/*	int mth[10];
+	
+for(int i=0;i<10;i++)
+
+{
+	cout<<"Enter The value";
+	cin>>mth[i];
+}
+
+
+cout<<mth[0]<<endl<<endl<<endl;
+
+
+for(int i=0;i<10;i++)
+{
+	cout<<mth[i]<<endl;
+}
+*/
+
+
+/*
+int arr[2];
+
+int mth[2] = {12,13};
+
+cout<<"First value is="<<mth[0]<<endl;
+cout<<"Second value is="<<mth[1]<<endl;
+
+*/
+/*int arr[3];
+
+cout<<"Enter First Value";
+cin>>arr[0];
+cout<<"Enter Second Value";
+cin>>arr[1];
+cout<<"Enter Third value";
+cin>>arr[2];
+
+cout<<"First value="<<arr[0];
+cout<<"Second value="<<arr[1];
+cout<<"Third value="<<arr[2];
+*/
+
+
+
+
+
+
+}
+
+
+
+/*{{{{{{{{{{{{{{{{{{{{{{{{[arraysss}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+
+	int a[15], i,  count;
+	bool found=0;
+	float average;
+	cout << "enter the numbers=\n";
+	cin >> count;
+	cout << "enter" <<" " <<count << " "<<"elements = ";
+	for (i = 0; i < count; i++) {
+		cin >> a[i];
+	}
+	int sum=0;
+	for (i = 0; i < count; i++)
+	{
+		sum += a[i];
+	}
+	average = sum / count;
+	{	cout << "average=" << average;
+	} 
+	int num;
+	cout << "search the number\n";
+	cin >> num;
+	for (i = 0; i < count; i++)
+	{
+		if (a[i] == num)
+		{
+		
+			cout << "element found at index=\n"<<i;
+			found==1;
+		}
+		
+	}
+	if (found==0)
+	
+	{
+		cout<<"Not found";
+	}
+	return 0;*/
+	
+	
+/*	int arr[2][3];
+	arr[0][1]=10;
+	arr[0][2]=20;
+	arr[0][3]=30;
+	arr[1][1]=40;
+	arr[1][2]=50;
+	arr[1][3]=60;*/
+	
+	
+/*	int arr[3][4]={{1,2,3,4},
+	{5,6,7,8},
+	{9,10,11,12}
+	};
+	cout<<arr[0][1];
+	*/
+	
+	int arr[3][4];
+	
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<4;j++)
+		{
+			cout<<"Enter value=";
+			cin>>arr[i][j];
+		}
+		
+		
+	}
+	
+	cout<<"After taking values"<<endl;
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				cout<<arr[i][j]<<endl;
+			}
+		}
+		
+		int max=arr[0][0];
+		int min=arr[0][0];
+		
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				if(arr[i][j]>max)
+				{
+					max=arr[i][j];
+				}
+				if(arr[i][j]<min)
+				{
+					min=arr[i][j];
+				}
+			}
+		}
+		cout<<"Maximum number is="<<max;
+		cout<<"Minimum number is="<<min;
+	
+}
+
+
+/*{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{arrays 2d}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+
+#include<iostream>
+using namespace std;
+main()
+{
+/*	int month;
+	int total;
+	int day;
+	int days_per_m[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	cout<<"Enter Current Month=";
+	cin>>month;
+	cout<<"Enter current day=";
+	cin>>day;
+	total=day;
+	for(int i=0;i<month-1;i++)
+	{
+		total=total+days_per_m[i];
+	}
+	cout<<"Total days till today in this year are="<<total;
+	*/
+	
+	
+/*	int arr[2][2];
+	int arrr[2][2];
+	cout<<"Taking values for first matrix"<<endl;
+	for(int i=0;i<=1;i++)
+	{
+		for(int j=0;j<=1;j++)
+		{
+			cout<<"Enter value=";
+			cin>>arr[i][j];
+		}
+	}
+	cout<<"Taking values for 2nd matrix"<<endl;
+	for(int i=0;i<=1;i++)
+	{
+		for(int j=0;j<=1;j++)
+		{
+			cout<<"Enter value=";
+			cin>>arrr[i][j];
+		}
+	}
+	int sum_arr[2][2];
+	for(int i=0;i<=1;i++)
+	{
+		for(int j=0;j<=1;j++)
+		{
+			sum_arr[i][j]=arr[i][j]+arrr[i][j];
+		}
+	}
+	cout<<"resultant matrix is below"<<endl;
+	for(int i=0;i<=1;i++)
+	{
+		for(int j=0;j<=1;j++)
+		{
+			
+			cout<<sum_arr[i][j]<<endl;
+		}
+	}
+	int sum=0;
+	
+	for(int i=0;i<=1;i++)
+	{
+		for(int j=0;j<=1;j++)
+		{
+			if(i==j)
+			{
+				sum=sum+sum_arr[i][j];
+			}
+			
+		}
+	}
+	cout<<"Sum of diagonal enteries are="<<sum;
+	
+	
+	
+	
+	*/
+	
+	
+/*	int arr[4][7][3];
+	arr[0][0][0]=12;
+	cout<<arr[0][0][0];
+	*/
+	
+/*	for()
+	{
+	   for()
+	   {
+	   	for()
+	   	{
+	   		
+		}
+		}	
+	}
+
+
+
+
+*/
+
+/*for(int i=0;i<=5;i++){
+
+    if(i==4){
+       break;
+	}
+	cout<<i<<endl;
+}
+
+
+*/
+
+
+
+/*for(int i=0;i<=5;i++){
+
+    if(i==4){
+       continue;
+	}
+	cout<<i<<endl;
+	*/
+	
+	
+	
+}
+
+
+
+/*{{{{{{{{{{{{{{{{{{{FUNCTIONS}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+
+
+include<iostream>
+using namespace std;
+void show();//function prototype
+main()
+{
+	show();//function call
+	
+}
+void show()
+{
+	cout<<"My first function";
+
+}*/
+
+
+/*#include<iostream>
+using namespace std;
+void show()
+{
+	cout<<"First function";
+}
+
+main()
+{
+	
+show();
+cout<<"Function ends";
+
+}*/
+
+#include<iostream>
+using namespace std;
+/*void max_num(int x,int y){
+	if(x>y)
+	{
+		cout<<"a is greater than b"<<endl;
+		
+	}
+	else
+	{
+		cout<<"b is greater than a"<<endl;
+	}
+	
+}
+main()
+{
+	int a,b;
+	cout<<"Enter first value=";
+	cin>>a;
+	cout<<"enter second value=";
+	cin>>b;
+	max_num(a,b);
+	*/
+	void tablee(int x)
+	{
+	for(int i=1;i<11;i++)
+	{
+		cout<<x*i<<endl;
+		}	
+	}
+	
+main()
+	
+{ int a;
+cout<<"Enter number=";
+cin>>a;
+tablee(a);
+
+}
+
+/*{{{{{{{{{{{{{{{{{{{{functions}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}]]
+
+
+#include<iostream>
+using namespace std;
+int sqr(int n)
+{
+	return n*n;
+}
+int cube(int m)
+{
+	return m*m*m;
+}
+main()
+{int a,b;
+int x,y,z;
+	cout<<"Enter First integer=";
+	cin>>a;
+	cout<<"Enter second value=";
+	cin>>b;
+	x=sqr(a);
+	y=cube(b);
+	z=x+y;
+	cout<<"Sum is="<<z<<endl;
+}*/
+
+/*#include<iostream>
+using namespace std;
+void swap(int &x,int &y)
+{
+	int t;
+	t=x;
+	x=y;
+	y=t;
+}
+main()
+{
+	int a,b;
+	cout<<"enter first value=";
+	cin>>a;
+	cout<<"enter second value=";
+	cin>>b;
+	cout<<"Before swapping";
+	cout<<"Value of a="<<a<<endl;
+	cout<<"value of b="<<b<<endl;
+	
+	swap(a,b);
+	
+	cout<<"After swapping";
+	cout<<"Value of a="<<a<<endl;
+	cout<<"value of b="<<b<<endl;
+}*/
+
+/*#include<iostream>
+using namespace std;
+void um(int j)
+{
+	cout<<"value is="<<j;
+}
+main()
+{
+	for(int i=0;i<5;i++)
+	{
+		um(i);
+	}
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*{{{{{{{{{{{{{{{{{{{{{{{{{[[[[structures}}}}}}}}}}}}}}}}}}}}}}}}}]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+/*#include<iostream>
+using namespace std;
+struct students
+{
+	int rollno;
+	float avg;
+	int marks;
+	char grade;
+	
+};
+
+main()
+{
+	students s;
+	cout<<"Enter rollno=";
+	cin>>s.rollno;
+	cout<<"Enter marks=";
+	cin>>s.marks;
+	cout<<"Enter grade=";
+	cin>>s.grade;
+	cout<<"Enter average of marks=";
+	cin>>s.avg;
+	cout<<"You entered this data"<<endl<<endl;
+	cout<<"Rollno="<<s.rollno<<endl;
+	cout<<"Marks="<<s.marks<<endl;
+	cout<<"avg="<<s.avg<<endl;
+	cout<<"grades="<<s.grade<<endl;
+	
+}*/
+
+
+
+/*#include<iostream>
+using namespace std;
+struct birthday
+{
+	int day;
+	int month;
+	int year;
+};
+
+main()
+{   birthday s;
+	cout<<"Enter day of birth=";
+	cin>>s.day;
+	cout<<"Enter your month=";
+	cin>>s.month;
+	cout<<"Enter year=";
+	cin>>s.year;
+	cout<<s.day<<"/"<<s.month<<"/"<<s.year;
+}
+*/
+
+
+/*#include<iostream>
+using namespace std;
+struct students
+{
+	int marks;
+	float avg;
+};
+main()
+{
+	students s={13,45.5};
+	cout<<s.avg;
+	
+}
+
+
+*/
+
+
+
+
+/*#include<iostream>
+using namespace std;
+struct students
+{
+	int rollno;
+	int marks[6];
+	char grade;
+	float avg;
+};
+main()
+{
+	students s;
+	int sum=0;
+cout<<"Enter your rollno=";
+cin>>s.rollno;
+cout<<"Enter your grade=";
+cin>>s.grade;
+for(int i=0; i<6;i++)
+{
+	cout<<"Add your marks=";
+    cin>>s.marks[i];
+    sum=sum+s.marks[i];
+}
+s.avg=sum/6;
+cout<<"Average of your marks is="<<s.avg;
+
+}*/
+
+
+
+
+
+
+
+//{{{{{{{{{{{{{{{{{{{{{[classsssss]}}}}}}}}}}}}}}}}}}}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+class practice
+{
+private:
+	int n;
+public:
+	void inputS()
+	{
+		cout<<"Enter the number=";
+		cin>>n;
+		cout<<endl;
+		}
+	void outputs()
+	{
+		cout<<"The entered number is="<<n<<endl;
+			}		
+};
+main()
+{
+	practice p1;
+	p1.inputS();
+	p1.outputs();
+}
+
+*/
+
+
+/*#include<iostream>
+using namespace std;
+class marks
+{
+	private:
+		int a,b,c;
+	public:
+		int f;
+		void input()
+		{
+			cout<<"Enter marks of first subject=";
+			cin>>a;
+			cout<<"Enter marks of second subject=";
+			cin>>b;
+			cout<<"Enter marks of third subject=";
+			cin>>c;
+		}
+		int sum()
+		{
+			f=a+b+c;
+			return f;
+		}
+		float avg()
+		{
+			float av;
+			av=(f)/3;
+			return av;
+		}
+	
+};
+main()
+{int s;float avg1;
+	marks m1;
+	m1.input();
+	s=m1.sum();
+	avg1=m1.avg();
+	cout<<"Sum of numbers="<<s<<endl;
+	cout<<"Average of numbers="<<avg1<<endl;
+}
+
+*/
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+class book
+{
+private:
+int id,price,pages;	
+public:
+	void inputs()
+	{
+	cout<<"Enter book id=";
+	cin>>id;
+	cout<<"Enter no of pages=";
+	cin>>pages;
+	cout<<"Enter price of book=";
+	cin>>price;
+	}
+	void outputs()
+	{
+		cout<<"Book id="<<id<<endl;
+		cout<<"Price of book="<<price<<endl;
+		cout<<"Pages of book="<<pages<<endl;
+	}
+	void val(int id,int price, int pages)
+	{
+		cout<<"Id="<<id<<endl;
+		cout<<"price="<<price<<endl;
+		cout<<"pages="<<pages<<endl;
+	}
+	
+};
+main()
+{
+	book b1,b2;
+	b1.inputs();
+	b1.outputs();
+	b2.inputs();
+	b2.outputs();
+	b2.val(2,100,1000);
+	
+	
+}
+
+
+#include<iostream>
+using namespace std;
+class name1
+{private:
+	int n;
+	void show();
+	
+};
+void name1::show()
+{for(int i=1;i<10;i++)
+{
+	cout<<i<<endl;
+}
+	
+}
+main()
+{
+	name1 obj1;
+	cout<<"Starting 10 natural numbers="<<endl;
+	obj1.show();
+}
+*/
+
+
+//{{{{{{{{{[{{{{{{{{{{{{{{{{{{{{{connstructor}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+
+//1 not return type
+
+//2/jo class ka name hoga whe constructor ka hpga
+
+//3 constructor khudi call hojata hai jb object bnta hai
+
+
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+class name1
+{
+    private:
+	  int n;
+	public:
+	  void show();
+	
+};
+void name1::show()
+{for(int i=1;i<10;i++)
+{
+	cout<<i<<endl;
+}
+	
+}
+main()
+{
+	name1 obj1;
+	cout<<"Starting 10 natural numbers="<<endl;
+	obj1.show();
+}*/
+
+/*#include<iostream>
+using namespace std;
+class firsts
+{
+	private:
+		int n;
+	public:
+	    firsts()
+	   {
+	   	cout<<"This is a constructor"<<endl;
+		   }	
+};
+main()
+{
+	firsts f1,f2,f3;
+	cout<<"My first statement outside constructor"<<endl;
+	firsts f4;
+}
+
+
+#include<iostream>
+using namespace std;
+class firsts
+{
+private:
+  int x,y;
+public:
+ firsts()
+ {
+ 	x=10;
+ 	y=20;
+ 	cout<<"Constructor is being called"<<endl;
+	 }
+  void sum()
+  {
+  	int sum=x+y;
+  	cout<<"Sum of both numbers="<<sum<<endl;
+	  }	
+};
+main()
+{
+	firsts f1;
+	f1.sum();
+	firsts();
+}
+
+
+
+//{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{constructor overloading or destructor}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}]
+
+
+
+/*#include<iostream>
+using namespace std;
+class practice
+{
+	private:
+		int n;
+	public:
+	practice()
+	{
+		cout<<"COnstructor without parameter"<<endl;
+	}
+	practice(int m)
+	{
+		cout<<"Constructor with paramters"<<endl;
+		cout<<"Value of m="<<m;
+	}
+	
+};
+
+main()
+{
+	practice m;
+	practice m1(10);
+	
+	
+}*/
+
+
+
+
+
+
+/*
+#include<iostream>
+using namespace std;
+class first
+{
+	private:
+		int n;
+	public:
+		first()
+		{
+			cout<<"This is a constructor"<<endl;
+			
+		}
+		~first()
+		{
+			cout<<"This is a destructor"<<endl;
+		}
+};
+main()
+{
+	first a,b;
+	cout<<"NAME IS="<<endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{[[[[[[[questions of class and constructor}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}]
+
+
+
+
+
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+class work
+{
+	private:
+		int n;
+	public:
+		work()
+		{
+			cout<<"First constructor withour parameters"<<endl;
+		}
+		work(int a,int b)
+		{
+			int sums=a+b;
+			cout<<"Sum is="<<sums<<endl;
+			
+		}
+		work(int sum)
+		{
+			float avg=sum/2;
+			cout<<"Average of two numbers="<<avg<<endl;
+		}
+	
+};
+main()
+{int x,y;
+work l;
+	cout<<"Enter first value=";
+	cin>>x;
+	cout<<"Enter Second value=";
+	cin>>y;
+	int sumss=x+y;
+	work m(x,y);
+	work n(sumss);
+	
+	
+}*/
+
+/*#include<iostream>
+using namespace std;
+class book
+{
+	private:
+		int page,pr;
+	public:
+		void get()
+		{
+		cout<<"Enter pages=";
+		 cin>>page;
+		 cout<<"Enter price=";
+		 cin>>pr;
+			
+		}
+		void show()
+		{
+			
+			cout<<"Pages="<<page<<endl;
+			cout<<"Price="<<pr<<endl;
+		}
+	
+};
+main()
+{
+	book b1;
+	b1.get();
+	book b2(b1);
+	book b3=b1;
+	cout<<"The data of b1="<<endl;
+	b1.show();
+	cout<<"The data of b2="<<endl;
+	b2.show();
+	cout<<"The data of b3="<<endl;
+	b3.show();
+}*/
+
+
+
+/*#include<iostream>
+using namespace std;
+class travel
+{
+	private:
+		int km,hr;
+	public:
+		travel()
+		{ 
+		km=hr=0;
+			
+		}
+		void get()
+		{
+			cout<<"Enter kms travelled=";
+			cin>>km;
+			cout<<"Enter hours=";
+			cin>>hr;
+			
+		}
+		void show()
+		{
+			cout<<"You travelled "<<km<<" kms in "<<hr<<" hours"<<endl;
+		}
+		void add(travel p)
+		{
+		travel t;
+		t.km=km+p.km;
+		t.hr=hr+p.hr;
+		cout<<"Total traveeling is"<<t.km<<" kms in "<<t.hr<<" hours"<<endl;
+		}
+	
+};
+main()
+{
+	travel my, your;
+	my.get();
+	my.show();
+	your.get();
+	your.show();
+	my.add(your);
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*{{{{{{{{{{{{{{{{{{{class in class}}}}}}}}}}}}}}}}}}
+
+
+
+
+
+
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+class B;
+class A
+{
+	private:
+		int n=10;
+	public:
+		void show()
+		{
+			cout<<n;
+		}
+		
+	friend void print(A,B);
+};
+class B
+{private:
+	int n=20;
+ public:
+	void show()
+	{
+		cout<<n;
+	}
+	friend void print(A,B);
+	
+};
+void print(A obj,B obj3)
+{
+	int sum;
+	sum=obj.n+obj3.n;
+	cout<<"Value of n in class A="<<obj.n<<endl;
+	cout<<"value of n in class B="<<obj3.n<<endl;
+	cout<<"Sum of both values="<<sum;
+	
+}
+main()
+{A obj1;
+ B obj2;
+ print(obj1,obj2);
+ 
+ 	
+}
+*/
+
+/*#include<iostream>
+using namespace std;
+class A
+{
+	private:
+		int a,b;
+	public:
+		A()
+		{
+			a=10;
+			b=20;
+		}
+	friend class B;
+};
+class B
+{
+	public:
+		void show(A obj)
+		{
+			cout<<"The value of a="<<obj.a<<endl;
+			cout<<"The value of b="<<obj.b<<endl;
+		
+		}
+		void print(A obj1)
+		{
+			cout<<"The value of a="<<obj1.a<<endl;
+		}
+	
+};
+main()
+{
+	A obj1;
+	B obj2;
+	obj2.show(obj1);
+	obj2.print(obj1);
+	
+	
+}*
+
